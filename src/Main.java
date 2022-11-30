7857486d42707aa6438c143d3417d23ba9080bc3
klimchess <vanyusha.1996@mail.ru>
2022-11-30 12:54:20 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.LinkedTransferQueue;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 2000;
+        System.out.println("Значение переменной с типом int равно " + a );
+        byte b = 1;
+        System.out.println("Значение переменной с типом byte равно " + b);
+        short c = 85;
+        System.out.println("Значение пересенной с типом short равно " + c);
+        long d = -8000;
+        System.out.println("Значение переменной с типом long равно " + d);
+        float f = 4.5F;
+        System.out.println("Значение переменной с типос float равно " + f);
+        double e = 65.74;
+        System.out.println("Значeние переменной с типом double равно " + e);
+
+    }
+}
